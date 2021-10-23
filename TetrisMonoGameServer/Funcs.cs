9a0985cafd9edb16a32871f8b8b40c4e07effd65
Guido Mintjes <guidomintjes@gmail.com>
2021-10-23 18:59:54 +0200

    Cool, je kan nu 1 bericht verzenden en dan crasht de verbinding

--- TetrisMonoGameServer/Funcs.cs
+++ TetrisMonoGameServer/Funcs.cs
@@ -1,144 +1,50 @@
 ﻿using System;
-using System.Threading;
 
-namespace TCPChatServer {
+namespace TetrisMonoGameServer {
+
     public static class Funcs {
 
         private static string errorAlert = "[ERROR] ";
         private static string warningAlert = "[WARNING] ";
         private static string messageAlert = "[MSG] ";
         private static string serverAlert = "[SERVER] ";
-        private static string chatAlert = "[CHAT] ";
 
-        private static bool allowTypeWrite = false;
 
-        public static void printMessage(int alertLevel, string message, bool typeWrite) {
+        public static void printMessage(int alertLevel, string message) {
 
-            if (ServerCommand.reading) {
+            switch (alertLevel) {
+                case 0:
 
-                Thread T1 = new Thread(() => printer(alertLevel, message, typeWrite));
+                    string msgErr = errorAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
 
-                T1.Start();
+                    Console.WriteLine(msgErr);
 
-            } else {
+                    break;
 
-                printer(alertLevel, message, typeWrite);
+                case 1:
+                    string msgWarn = warningAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
+
+                    Console.WriteLine(msgWarn);
+
+                    break;
+
+                case 2:
+                    string msgMsg = messageAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
+
+                    Console.WriteLine(msgMsg);
+
+                    break;
+
+                case 3:
+                    string msgServer = serverAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
+
+                    Console.WriteLine(msgServer);
+
+                    break;
+
+                default:
+                    break;
             }
-        }
-
-
-        private static void printer(int alertLevel, string message, bool typeWrite) {
-
-
-            if (allowTypeWrite) {
-                switch (alertLevel) {
-                    case 0:
-
-                        string msgErr = errorAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
-
-                        if (typeWrite)
-                            slowType(msgErr, 3);
-                        else
-                            Console.WriteLine(msgErr);
-
-                        break;
-
-                    case 1:
-                        string msgWarn = warningAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
-
-                        if (typeWrite)
-                            slowType(msgWarn, 3);
-                        else
-                            Console.WriteLine(msgWarn);
-
-                        break;
-
-                    case 2:
-                        string msgMsg = messageAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
-
-                        if (typeWrite)
-                            slowType(msgMsg, 3);
-                        else
-                            Console.WriteLine(msgMsg);
-
-                        break;
-
-                    case 3:
-                        string msgServer = serverAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
-
-                        if (typeWrite)
-                            slowType(msgServer, 3);
-                        else
-                            Console.WriteLine(msgServer);
-
-                        break;
-
-                    case 4:
-                        string msgChat = chatAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
-
-                        if (typeWrite)
-                            slowType(msgChat, 3);
-                        else
-                            Console.WriteLine(msgChat);
-
-                        break;
-
-                    default:
-                        break;
-                }
-            } else {
-
-                switch (alertLevel) {
-                    case 0:
-
-                        string msgErr = errorAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
-
-                        Console.WriteLine(msgErr);
-
-                        break;
-
-                    case 1:
-                        string msgWarn = warningAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
-
-                        Console.WriteLine(msgWarn);
-
-                        break;
-
-                    case 2:
-                        string msgMsg = messageAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
-
-                        Console.WriteLine(msgMsg);
-
-                        break;
-
-                    case 3:
-                        string msgServer = serverAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
-
-                        Console.WriteLine(msgServer);
-
-                        break;
-
-                    case 4:
-                        string msgChat = chatAlert + "{" + DateTime.Now.ToString("HH:mm:ss") + "} " + message;
-
-                        Console.WriteLine(msgChat);
-
-                        break;
-
-                    default:
-                        break;
-                }
-            }
-        }
-
-
-
-        public static void slowType(string message, int delay) {
-            foreach (char character in message) {
-                Console.Write(character);
-                System.Threading.Thread.Sleep(delay);
-            }
-            Console.Write("\n");
         }
     }
 }
---
+++ TetrisMonoGameServer/TetrisMonoGameServer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TetrisMonoGameServer {
+    class TetrisMonoGameServer {
+
+
+        // Create the class that listens for all incoming connections
+        static TcpListener server;
+
+
+        // Related to connecting
+        static IPAddress localIP = IPAddress.Parse("127.0.0.1");
+        static int port;
+
+
+        static bool listening = true;
+
+
+        // Initialize server
+        public static void Main(string[] args) {
+
+            // Loads server settings from a text file so settings can be changed without
+            // building every time
+            string readServerSettings = File.ReadAllText("SERVER_SETTINGS.txt");
+            string[] serverSettings = readServerSettings.Split(";");
+
+
+            // Loads the settings from the stringarray, indices explained in SETTINGS_EXPLAINED.txt
+            port = int.Parse(serverSettings[0]);    
+
+
+            // Create the TcpListener with the correct IP address and port and 
+            //  start listening for incoming connections (aka start the server)
+            server = new TcpListener(localIP, port);
+            server.Start();
+
+
+            // Create the datastream variable in the DataStream class
+            DataStream.dataStream = new byte[256];  // This will be the length of the datastream
+
+
+            // Enter the loop that will listen for incoming connections
+            ListenLoop();
+
+            Console.ReadLine();
+        }
+
+
+        static void ListenLoop() {
+
+            // This method will make sure incoming connections are accepted and the connection is stored
+            //  to an instance of the TcpClient class
+            TcpClient client = server.AcceptTcpClient();
+
+            Funcs.printMessage(3, "Incoming connection!");
+
+            DataStream.networkStream = client.GetStream();
+
+            DataStream.networkStream.Read(DataStream.dataStream, 0, DataStream.dataStream.Length);
+
+            Console.WriteLine(DataStream.ReadString(false));
+
+            if(listening)
+                ListenLoop();
+        }
+    }
+}
