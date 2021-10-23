e165047191038109b3a80a7b2fa369afbf7f51af
Guido Mintjes <guidomintjes@gmail.com>
2021-10-23 18:59:54 +0200

    Cool, je kan nu 1 bericht verzenden en dan crasht de verbinding

---
+++ TetrisMonoGameServer/DataStream.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TetrisMonoGameServer {
+    class DataStream {
+
+        // The raw byte data stream and the string that the stream gets parsed into
+        public static byte[] dataStream;
+        public static string dataInStream;
+
+        public static int writePointer;
+        public static int readPointer;
+
+
+        // This is the 'actual' data stream
+        public static NetworkStream networkStream;
+
+
+        /// <summary>
+        /// This method will write a string to the byte array data stream
+        /// </summary>
+        public static string ReadString(bool moveReadPointer) {
+
+            string message = Encoding.ASCII.GetString(dataStream);  // Convert the message string to bytes
+
+            if(moveReadPointer)
+                readPointer += message.Length;
+
+            return message;
+        }
+    }
+}
